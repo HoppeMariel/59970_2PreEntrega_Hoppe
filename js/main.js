2adb27b500ba6581a1c73be4a06bcce3560a2151
HoppeMariel <mariel.hoppe@gmail.com>
2024-09-11 20:28:22 -0300

    grabo primer version

---
+++ js/main.js
@@ -0,0 +1,4 @@
+//! Hola Lucas, disculpá que entregue en BLANCO. 
+//? Tuve algunos problemas personales, me separé, me mudé, francamente, no tuve tiempo para dedicarle al curso.
+//todo Espero poder llegar a cumplir con la reentrega, sino tendré el desaprobado que merezco.
+//* Mil disculpas. Saludos
